61874589f61712bfd007691379188977e5f5c644
ameli72@hotmail.com <ameli72@hotmail.com>
2022-03-13 14:13:15 +0100

    Refactor Dependencies
    Move Module dependencies into a centralized location for easier management and to ensure version consistency

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,25 @@
+object Deps {
+    @JvmField
+    val androidx_core = "androidx.core:core-ktx:${Versions.androidx_core}"
+
+    @JvmField
+    val androidx_appCompat = "androidx.appcompat:appcompat:${Versions.androidx_appCompat}"
+
+    @JvmField
+    val materialDesign = "com.google.android.material:material:${Versions.materialDesign}"
+
+    @JvmField
+    val androidx_constraintLayout =
+        "androidx.constraintlayout:constraintlayout:${Versions.androidx_constraintLayout}"
+
+
+    @JvmField
+    val junit = "junit:junit:${Versions.jUnit}"
+
+    @JvmField
+    val androidx_test_junit = "androidx.test.ext:junit:${Versions.androidx_test_junit}"
+
+    @JvmField
+    val androidx_test_espresso =
+        "androidx.test.espresso:espresso-core:${Versions.androidx_test_espresso}"
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,13 @@
+object Versions {
+    const val kotlin = "1.6.10"
+
+    const val androidx_core = "1.7.0"
+    const val androidx_appCompat = "1.4.1"
+    const val materialDesign = "1.5.0"
+
+    const val androidx_constraintLayout = "2.1.3"
+
+    const val jUnit = "4.13.2"
+    const val androidx_test_junit = "1.1.3"
+    const val androidx_test_espresso = "3.4.0"
+}
